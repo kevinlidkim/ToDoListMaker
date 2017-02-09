9b6214d5536ed9756b0127958809b776f465e35d
kevin <kevin.li.3@stonybrook.edu>
2017-02-09 14:26:04 -0500

    wrote skeleton endpoint for saving

--- src/main/java/com/blurple/controller/HelloWorldController.java
+++ src/main/java/com/blurple/controller/HelloWorldController.java
@@ -1,4 +1,6 @@
 package com.blurple.controller;
+
+import com.blurple.models.ToDoList;
  
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -36,5 +38,12 @@ public class HelloWorldController {
 		mv.addObject("message", "DUHH");
 		mv.addObject("name", "YOOOO");
 		return mv;
+	}
+
+	@RequestMapping("/save")
+	public String saveList(ToDoList toDoList) {
+		System.out.println("the list");
+		System.out.println(toDoList);
+		return "success";
 	}
 }
